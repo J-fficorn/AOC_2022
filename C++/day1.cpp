0d6eee6c87274eca9222a565316e9c1bda7afff8
Gefficorn <67201794+J-fficorn@users.noreply.github.com>
2022-12-01 14:01:43 -0500

    Create day1.cpp

---
+++ C++/day1.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <string>
+#include <stdio.h>
+using namespace std;
+ifstream fin("text.in");
+bool part_one = false;
+
+int main() {
+    long start = time(NULL);
+    int cur_cals = 0, elves[100000], i = 0;
+    string s;
+    while (getline(fin, s)) {
+        if (s.size() <= 1) {
+            elves[i] = cur_cals;
+            cur_cals = 0;
+            i++;
+        } else cur_cals += stoi(s);
+    }
+    sort(elves, elves + i, [](int &a, int &b) -> bool {return a > b;});
+    if (part_one) cout << elves[0];
+    else cout << elves[0] + elves[1] + elves[2];
+    long end = time(NULL);
+    cout << endl << end - start << " ms" << endl;
+    return 0;
+}
