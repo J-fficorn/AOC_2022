53d1cc29dd41611b601c0976bf155ba24d376890
Gefficorn <67201794+J-fficorn@users.noreply.github.com>
2022-12-11 13:00:15 -0500

    Refactored and cleaned day07.cpp

--- C++/day07.cpp
+++ C++/day07.cpp
@@ -1,10 +1,8 @@
 #include <iostream>
 #include <fstream>
-#include <string>
 #include <sstream>
 #include <vector>
 #include <stdio.h>
-#include <algorithm>
 #include <chrono>
 #define ss stringstream
 using namespace std;
@@ -21,10 +19,9 @@ bool visited[500] = {false};
 int dfs_sum(int x) {
     int total_size = folders[x].size;
     visited[x] = true;
-    for (int i = 0; i < folders[x].children.size(); i++) { //visit dirs + files
+    for (int i = 0; i < folders[x].children.size(); i++) //visit dirs + files
         if (!visited[folders[x].children[i]])
             total_size += dfs_sum(folders[x].children[i]);
-    }
     folders[x].total_size = total_size;
     return total_size;
 }
@@ -36,32 +33,28 @@ int main() {
     file main = {0, 0, 0, 0, "/"}; folders.push_back(main);
     while (getline(fin, s)) {
         ss x(s);
-        while (getline(x, t, ' ')) {
-            cmd[n] = t; n++;
-        }
+        while (getline(x, t, ' ')) cmd[n++] = t;
         if (cmd[0] == "$" && cmd[1] == "cd") {
-            switch (cmd[2]):
+            file x = folders[folder_index];
+            switch (cmd[2].at(0)) {
                 case '/':
                     folder_index = 0; //return to /
                     break;
                 case '.':
-                    folder_index = folders[folder_index].parent_index; //return to parent
+                    folder_index = x.parent_index; //return to parent
                     break;
                 default:
-                    for (int i = 0; i < folders[folder_index].children.size(); i++) { //all children
-                        int x = folders[folder_index].children[i];
-                        if (folders[x].name == cmd[2])
-                            folder_index = folders[x].index; //find searched directory
-                    }
+                    for (int i = 0; i < x.children.size(); i++) //all children
+                        if (folders[x.children[i]].name == cmd[2])
+                            folder_index = x.children[i]; //find searched directory
                     break;
             } //cmd
-        } else { 
-            file dir;
-            dir.size = (cmd[0] == "dir") ? 0 : stoi(cmd[0]);
-            dir.index = num_files;
-            dir.parent_index = folder_index; 
-            ir.name = cmd[1];
-            dir.total_size = 0;
+        } else if (cmd[0] != "$") { 
+            file dir = {
+                (cmd[0] == "dir") ? 0 : stoi(cmd[0]), //size
+                num_files, folder_index, 0, //index, parent_index, total_size
+                cmd[1] //name
+            };
             folders.push_back(dir);
             folders[folder_index].children.push_back(dir.index);
             num_files++;
@@ -69,8 +62,7 @@ int main() {
         n = 0;
     }
     for (int i = 0; i < folders.size(); i++)
-        if (!visited[i]) folders[i].total_size = dfs_sum(i);
-        // print_file(i); //sum files
+        if (!visited[i]) folders[i].total_size = dfs_sum(i); //sum files
     if (part_one) {
         for (int i = 0; i < folders.size(); i++)
             if (folders[i].size == 0 && folders[i].total_size <= 100000)
@@ -78,7 +70,7 @@ int main() {
     } else {
         int min_max = folders[0].total_size, needed = min_max - (70000000 - 30000000);
         for (int i = 0; i < folders.size(); i++) {
-            if (folders[i].size == 0 && folders[i].total_size - needed > 0)
+            if (folders[i].size == 0 && folders[i].total_size > needed)
                 min_max = min(min_max, folders[i].total_size);
         }
         total_directory_size = min_max;
