9281ec5577b50e9b63b217a8f821311614b498f1
Gefficorn <67201794+J-fficorn@users.noreply.github.com>
2022-12-15 14:08:51 -0500

    Shortened code for part 1 and time for part 2

--- C++/day15.cpp
+++ C++/day15.cpp
@@ -17,25 +17,29 @@ struct sensor {
 vector<sensor> all_sensors;
 vector<pair<int, int>> ranges;
 unordered_set<int> beacons_in_line;
+unordered_set<long> a, b;
 
-constexpr inline bool in_bounds(int a, int upper) {
-    return a >= 0 && a <= upper;
-}
+constexpr inline bool in_bounds(int a, int upper) { return a >= 0 && a <= upper; }
 
 int main() {
     auto start = chrono::steady_clock::now();
     string s, t, input[10];
-    int n = 0, input_n = 0; //[0] minx, [1] maxx, [2] miny, [3] maxy;
+    int n = 0, input_n = 0;
     int x, y, b_x, b_y;
     while (getline(fin, s)) {
-        ss x(s);
-        while (getline(x, t, ' '))
+        ss line(s);
+        while (getline(line, t, ' '))
             input[input_n++] = t;
-        sensor temp_sensor = {stoi(input[2].substr(2, input[2].size() - 1 - 2)),
-                            stoi(input[3].substr(2, input[3].size() - 1 - 2)),
-                            stoi(input[8].substr(2, input[8].size() - 1 - 2)),
-                            stoi(input[9].substr(2))};
-        temp_sensor.man_dist = abs(temp_sensor.x - temp_sensor.b_x) + abs(temp_sensor.y - temp_sensor.b_y);
+        int x = stoi(input[2].substr(2, input[2].size() - 1 - 2)),
+            y = stoi(input[3].substr(2, input[3].size() - 1 - 2)),
+            b_x = stoi(input[8].substr(2, input[8].size() - 1 - 2)),
+            b_y = stoi(input[9].substr(2)),
+            md = abs(x - b_x) + abs(y - b_y);
+        sensor temp_sensor = {x, y, b_x, b_y, md};
+        a.insert(y - x + md + 1);
+        a.insert(y - x - (md + 1)); //pos
+        b.insert(y + x + md + 1);
+        b.insert(y + x - (md + 1)); //neg
         all_sensors.push_back(temp_sensor);
         input_n = 0;
         n++;
@@ -50,58 +54,38 @@ int main() {
             if (cur_sensor.b_y == row)
                 beacons_in_line.insert(cur_sensor.b_x);
             ranges.push_back(make_pair(cur_sensor.x - radius, cur_sensor.x + radius));
-            // printf("%d: %d, %d, %d\n", i, cur_sensor.x, cur_sensor.x - radius, cur_sensor.x + radius);
         }
         sort(ranges.begin(), ranges.end());
         pair<int, int> range = ranges[0];
         for (int i = 1; i < ranges.size(); i++) {
-            if (ranges[i].second < range.first) {
+            if (ranges[i].second < range.first || ranges[i].first > range.second) {
                 total_range += range.second - range.first + 1;
-                range.first = ranges[i].first; range.second = ranges[i].second;
-                continue;
-            } else if (ranges[i].first > range.second) {
-                total_range += range.second - range.first + 1;
-                range.first = ranges[i].first; range.second = ranges[i].second;
+                range = ranges[i];
                 continue;
             }
-            range.first = min(range.first, ranges[i].first);
-            range.second = max(range.second, ranges[i].second);
+            range = make_pair(min(range.first, ranges[i].first), max(range.second, ranges[i].second));
         }
         total_range += range.second - range.first + 1 - beacons_in_line.size();
     } else {
         int upper = 4000000;
         vector<pair<int, int>> possible_locations;
-        for (int i = 0; i < n; i++) {
-            sensor ssr = all_sensors[i];
-            int new_man = ssr.man_dist + 1;
-            for (int j = new_man; j >= 0; j--) {
-                int l_x = ssr.x - (new_man - j), r_x = ssr.x + (new_man - j),
-                    t_y = ssr.y + j, b_y = ssr.y - j;
-                if (in_bounds(l_x, upper)) {
-                    if (in_bounds(t_y, upper)) possible_locations.push_back(make_pair(l_x, t_y));
-                    if (in_bounds(b_y, upper)) possible_locations.push_back(make_pair(l_x, b_y));
-                }
-                if (in_bounds(r_x, upper)) {
-                    if (in_bounds(t_y, upper)) possible_locations.push_back(make_pair(r_x, t_y));
-                    if (in_bounds(b_y, upper)) possible_locations.push_back(make_pair(r_x, b_y));
-                }
+        for (auto s_a : a) {
+            for (auto s_b : b) {
+                int x = (s_b - s_a) / 2, y = (s_b + s_a) / 2;
+                if (in_bounds(x, upper) && in_bounds(y, upper))
+                    possible_locations.push_back(make_pair(x, y));
             }
-            //trace man + 1
         }
-        for (int i = 0; i < possible_locations.size(); i++) {
+        for (auto p : possible_locations) {
             bool valid = true;
-            for (int j = 0; j < n; j++) {
-                sensor cur_sensor = all_sensors[j];
-                int cur_man_dist = abs(cur_sensor.x - possible_locations[i].first)
-                                 + abs(cur_sensor.y - possible_locations[i].second);
-                if (cur_sensor.man_dist >= cur_man_dist) valid = false;
-            }
-            if (valid)
-                total_range = 4000000 * ((long) possible_locations[i].first) + possible_locations[i].second;
+            for (auto ssr : all_sensors)
+                if (ssr.man_dist >= (abs(ssr.x - p.first) + abs(ssr.y - p.second)))
+                    valid = false;
+            if (valid) total_range = 4000000 * (long) p.first + p.second;
         }
     }
     cout << total_range;
     auto elapsed = chrono::duration_cast<std::chrono::nanoseconds>(chrono::steady_clock::now() - start);
-    printf("\n%.2f ms\n", elapsed.count()*1e-6);
+    printf("\n%.4f ms\n", elapsed.count()*1e-6);
     return 0;
 }
